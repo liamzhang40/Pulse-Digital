da2a5b45f7cecf5444f45d8627db1c00fe4f2cd5
Liangtian Zhang <lzhang40@binghamton.edu>
2018-09-22 17:28:01 -0400

    completed webpack setup

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardBody from './components/card_body';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  ReactDOM.render(<div>Hello World</div>, root);
+});
---
+++ src/components/card_body.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class CardBody extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      influencerType: "",
+      indicationCategory: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default CardBody;
